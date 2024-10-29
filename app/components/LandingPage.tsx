6cd6033c53d8aba66574497cb5b429fe52cff808
Yoshi Debat <yoshiwebdev@gmail.com>
2024-10-28 23:29:56 -0300

    translate texts and add game images

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Menu, X, Monitor, Gauge, Users } from "lucide-react";
+import { Menu, X, Sparkles, Scale3D, UsersRound } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import LogoDataCenterGaming from "../assets/logo_datacenter_gaming2.png";
 import GameImage from "../assets/game2.jpg";
-import GolfImage from "../assets/golf.jpg";
+import GolfImage from "../assets/golf2.png";
 
 export default function LandingPage() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,17 +18,24 @@ export default function LandingPage() {
       <header className="bg-blue-800 text-white">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
           <Link href="/" className="flex items-center space-x-2">
+            <div>
+              <Image
+                src={LogoDataCenterGaming}
+                height={40}
+                alt="Logo Datacenter Gaming"
+              />
+            </div>
             <span className="text-xl font-bold">Datacenter Gaming</span>
           </Link>
           <nav className="hidden md:flex space-x-4">
             <Link href="#features" className="hover:text-blue-300">
-              Features
+              Características
             </Link>
             <Link href="#about" className="hover:text-blue-300">
-              About
+              Sobre el Juego
             </Link>
             <Link href="#contact" className="hover:text-blue-300">
-              Contact
+              Novedades
             </Link>
           </nav>
           <Button
@@ -46,13 +54,13 @@ export default function LandingPage() {
         {isMenuOpen && (
           <nav className="md:hidden bg-blue-700 p-4">
             <Link href="#features" className="block py-2 hover:text-blue-300">
-              Features
+              Características
             </Link>
             <Link href="#about" className="block py-2 hover:text-blue-300">
-              About
+              Sobre el Juego
             </Link>
             <Link href="#contact" className="block py-2 hover:text-blue-300">
-              Contact
+              Novedades
             </Link>
           </nav>
         )}
@@ -72,40 +80,53 @@ export default function LandingPage() {
             <div className="absolute inset-0 bg-slate-950 opacity-70"></div>
           </div>
           <div className="relative z-10 container mx-auto px-4 text-center text-white">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">Experience Golf Like Never Before</h1>
-            <p className="text-xl mb-8">Datacenter Gaming presents the most immersive golf simulation ever created</p>
-            <Button className="bg-blue-600 hover:bg-blue-700 text-white">Pre-order Now</Button>
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+              Experimentá el Golf Como Nunca Antes
+            </h1>
+            <p className="text-xl mb-8">
+              Datacenter Gaming presenta <b>Golfito Elemental</b>, un nuevo
+              videojuego, una nueva experiencia
+            </p>
+            <Button className="bg-blue-600 hover:bg-blue-700 text-white">
+              Pre-ordená Ahora
+            </Button>
           </div>
         </section>
 
         <section id="features" className="py-28">
           <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold mb-12 text-center">
-              Game Features
+              Características
             </h2>
             <div className="grid md:grid-cols-3 gap-8">
               <div className="text-center">
-                <Monitor className="h-16 w-16 mx-auto mb-4 text-blue-600" />
+                <Sparkles className="h-16 w-16 mx-auto mb-4 text-blue-600" />
                 <h3 className="text-xl font-semibold mb-2">
-                  Realistic Graphics
+                  Estilo Visual Atractivo
                 </h3>
                 <p>
-                  Experience stunning, lifelike golf courses from around the
-                  world
+                  Disfruta de un mundo vibrante y estilizado en low-poly que
+                  combina simplicidad y belleza
                 </p>
               </div>
               <div className="text-center">
-                <Gauge className="h-16 w-16 mx-auto mb-4 text-blue-600" />
-                <h3 className="text-xl font-semibold mb-2">Advanced Physics</h3>
-                <p>Feel every swing with our state-of-the-art physics engine</p>
-              </div>
-              <div className="text-center">
-                <Users className="h-16 w-16 mx-auto mb-4 text-blue-600" />
+                <Scale3D className="h-16 w-16 mx-auto mb-4 text-blue-600" />
                 <h3 className="text-xl font-semibold mb-2">
-                  Multiplayer Tournaments
+                  Físicas Realistas
                 </h3>
                 <p>
-                  Compete with players worldwide in exciting online tournaments
+                  Experimentá una mecánica de golf con físicas equilibradas que
+                  hacen que cada golpe sea emocionante y desafiante
+                </p>
+              </div>
+              <div className="text-center">
+                <UsersRound className="h-16 w-16 mx-auto mb-4 text-blue-600" />
+                <h3 className="text-xl font-semibold mb-2">
+                  Modo Casual y Competitivo
+                </h3>
+                <p>
+                  Alterná entre un modo casual, ideal para disfrutar con amigos,
+                  y un modo competitivo que desafía a los jugadores
                 </p>
               </div>
             </div>
@@ -125,17 +146,18 @@ export default function LandingPage() {
                 />
               </div>
               <div className="md:w-1/2 md:pl-12">
-                <h2 className="text-3xl font-bold mb-4">About Our Golf Game</h2>
+                <h2 className="text-3xl font-bold mb-4">Sobre el Juego</h2>
                 <p className="mb-4">
-                  Datacenter Gaming&apos;s latest creation brings the thrill of golf
-                  right to your living room. With cutting-edge graphics,
-                  realistic physics, and an immersive multiplayer experience,
-                  our golf game sets a new standard in sports simulation.
+                  Golfito Elemental es un videojuego de minigolf acerca de la
+                  autosuperación e ingenio, donde el jugador deberá sortear
+                  obstáculos haciendo uso de los elementos de fuego y agua para
+                  poder superar niveles cada vez más desafiantes, cuyas
+                  victorias son cada vez más gratificantes.
                 </p>
                 <p>
-                  Whether you&apos;re a seasoned pro or new to the blue, our game
-                  offers something for everyone. Practice your swing, compete in
-                  tournaments, or just enjoy a relaxing round with friends.
+                  El desafío se presenta en forma de rompecabezas, donde la
+                  experimentación de las físicas de la pelota y los distintos
+                  elementos entre sí será lo que lleve al jugador a la victoria.
                 </p>
               </div>
             </div>
@@ -144,23 +166,23 @@ export default function LandingPage() {
 
         <section id="contact" className="py-28">
           <div className="container mx-auto px-4 text-center">
-            <h2 className="text-3xl font-bold mb-8">Stay Updated</h2>
+            <h2 className="text-3xl font-bold mb-8">Mantente Actualizado</h2>
             <p className="mb-8">
-              Sign up for our newsletter to receive the latest news and
-              exclusive offers
+              Suscribite a nuestro boletín para recibir las últimas noticias y
+              ofertas exclusivas.
             </p>
             <form className="max-w-md mx-auto">
               <div className="flex gap-4">
                 <Input
                   type="email"
-                  placeholder="Enter your email"
+                  placeholder="Ingresá tu email"
                   className="flex-grow"
                 />
                 <Button
                   type="submit"
                   className="bg-blue-600 hover:bg-blue-700 text-white"
                 >
-                  Subscribe
+                  Suscribirse
                 </Button>
               </div>
             </form>
@@ -173,23 +195,31 @@ export default function LandingPage() {
           <div className="md:flex justify-between items-center">
             <div className="mb-4 md:mb-0">
               <Link href="/" className="flex items-center space-x-2">
+                <div>
+                  <Image
+                    src={LogoDataCenterGaming}
+                    height={40}
+                    alt="Logo Datacenter Gaming"
+                  />
+                </div>
                 <span className="text-lg font-semibold">Datacenter Gaming</span>
               </Link>
             </div>
             <nav className="flex flex-wrap justify-center md:justify-end gap-4">
               <Link href="#" className="hover:text-blue-300">
-                Privacy Policy
+                Política de privacidad
               </Link>
               <Link href="#" className="hover:text-blue-300">
-                Terms of Service
+                Terminos del Servicio
               </Link>
               <Link href="#" className="hover:text-blue-300">
-                Contact Us
+                Contacto
               </Link>
             </nav>
           </div>
           <div className="mt-8 text-center text-sm">
-            © {new Date().getFullYear()} Datacenter Gaming. All rights reserved.
+            © {new Date().getFullYear()} Datacenter Gaming. Todos los derechos
+            reservados.
           </div>
         </div>
       </footer>
